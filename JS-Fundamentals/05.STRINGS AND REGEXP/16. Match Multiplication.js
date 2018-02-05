f2e2b877286da74d114eb13296c9a3f4eb9d9963
IvanKrivulev <vande9855@gmail.com>
2018-02-06 00:39:24 +0200

    finished lab

--- JS-Fundamentals/05.STRINGS AND REGEXP/16. Match Multiplication.js
+++ JS-Fundamentals/05.STRINGS AND REGEXP/16. Match Multiplication.js
@@ -1,5 +1,5 @@
 function match(text) {
-    let regex = /([-\d]+)\s*\*\s*([.\d]+)/g;
+    let regex = /([-\d]+)\s*\*\s*([.\d-]+)/g;
 
     function replace(match, g1, g2) {
         return Number(g1) * Number(g2);
@@ -9,4 +9,4 @@ function match(text) {
     console.log(text);
 }
 
-match("My bill: 2*2.50 (beer); 2* 1.20 (kepab); -2 * 0.5 (deposit).");
+match("My bill is: 4 * 2.50 (beer); 12* 1.50 (kepab); 1  *4.50 (salad); 2  * -0.5 (deposit).");
